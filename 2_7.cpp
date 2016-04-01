bcadf25a81d486f45147a5bd7e05084340aae6dd
Stan Wang <stanjiawang@163.com>
2016-04-01 11:48:00 -0700

    Added files via upload

---
+++ 2_7.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+
+using namespace std;
+
+void showTime(int hours, int minutes);
+
+int main()
+{
+	int hours = 0;
+	int minutes = 0;
+
+	cout << "Enter the number of hours: ";
+	while(!(cin >> hours) || (0 > hours) || (24 < hours))	//check if the input is a float
+	{
+			cout << "Error!" << endl << "Please enter a positive number between 0 and 24: ";
+			cin.clear();	//set the ios_base::goodbit
+			cin.sync();	//clear the input stream
+	}
+
+	cout << "Enter the number of minutes: ";
+	while(!(cin >> minutes) || (0 > minutes) || (60 < minutes))	//check if the input is a float
+	{
+		cout << "Error!" << endl << "Please enter a positive number between 0 and 60: ";
+		cin.clear();	//set the ios_base::goodbit
+		cin.sync();	//clear the input stream
+	}
+
+	showTime(hours, minutes);
+	
+	system("pause");
+	return 0;
+}
+
+void showTime(int hours, int minutes)
+{
+	cout << "Time: " << hours << ":" << minutes << endl;
+}
