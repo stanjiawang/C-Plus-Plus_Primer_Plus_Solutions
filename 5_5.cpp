bb69742f762b9f0bbd540f97e02c89e46e00d01f
Stan Wang <stanjiawang@163.com>
2016-04-05 17:04:12 -0700

    Added files via upload

---
+++ 5_5.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+const int monthsPerYear = 12;
+
+int main()
+{
+	string months[monthsPerYear] = {"January", "Februaby", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"};
+	int sales[12] = {0};
+
+	for(int i = 0; i < monthsPerYear; i++)
+	{
+		cout << "Please enter the sales for " << months[i] << " : ";
+		while(!(cin >> sales[i]) || 0 > sales[i])
+		{
+			cout << "Error! Please enter a positive number : ";
+			cin.clear();
+			cin.sync();
+		}
+	}
+
+	for(int j = 0; j < monthsPerYear; j++)
+	{
+		cout << "The sales for " << months[j] << " is " << sales[j] << endl;
+	}
+
+	system("pause");
+	return 0;
+}
