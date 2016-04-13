6b6c4327926fa464a1ae57fa951a3f3c428e9675
Stan Wang <stanjiawang@163.com>
2016-04-13 12:09:40 -0700

    Added files via upload

---
+++ 6_1.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <cctype>
+
+int main()
+{
+	using namespace std;
+
+	char ch;
+
+	while(cin.get(ch) && '@' != ch)
+	{
+		if(!isdigit(ch))
+		{
+			if(islower(ch))
+				ch = toupper(ch);
+			else if(isupper(ch))
+				ch = tolower(ch);
+			else
+				continue;
+			cout << ch << endl;
+		}
+	}
+	system("pause");
+
+	return 0;
+}
